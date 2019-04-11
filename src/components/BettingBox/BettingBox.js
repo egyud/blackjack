9f0b79516d416675430cd7cb4dc7ce4eb29f3def
Andy <andy.egyud@gmail.com>
2019-04-11 09:56:25 -0700

    Improved styling of bettingBox

--- src/components/BettingBox/BettingBox.js
+++ src/components/BettingBox/BettingBox.js
@@ -7,12 +7,13 @@ const BettingBox = (props) => {
     let max = props.bank;
 
     return (
-      <form className={classes.BettingBox}>
-        <span>Bank: ${props.bank}</span>
-        <br/>
-        <input onChange={props.betChange} type="number" min="50" max={max} value={props.betAmount} step="50" disabled={props.gameOver}/>
-        <button onClick={props.allIn} disabled={props.gameOver}>All In</button>
-      </form>
+        <form className={classes.BettingBox}>
+          <h3>Place Your Bets Here</h3>
+          <span>Bank: ${props.bank}</span>
+          <br/>
+          <input onChange={props.betChange} type="number" min="50" max={max} value={props.betAmount} step="50" disabled={props.gameOver}/>
+          <button onClick={props.allIn} disabled={props.gameOver}>All In</button>
+        </form>
     )
 }
 
