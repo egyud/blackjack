da8bfbc5bcd3b42dde8889cb603012990f5ad9fa
Andy <andy.egyud@gmail.com>
2019-04-18 18:16:13 -0700

    Created build folder

---
+++ build/precache-manifest.e8504fcc0a725fd586956bda99990f2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da455e5a85cbb0d836ea",
+    "url": "/static/css/main.dfe68545.chunk.css"
+  },
+  {
+    "revision": "da455e5a85cbb0d836ea",
+    "url": "/static/js/main.da455e5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "8595fb8112fb57ee9c4f",
+    "url": "/static/js/2.8595fb81.chunk.js"
+  },
+  {
+    "revision": "6c77d73f2c190e3e9ef1f323cabfb298",
+    "url": "/index.html"
+  }
+];
